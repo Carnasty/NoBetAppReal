96be737eb1441e6f48d5ea6a05e2a03242a71caf
Carsen <cwerbes@gmail.com>
2025-08-05 15:18:38 -0700

    Home page complete

---
+++ NoBetAppReal/Views/MainApp Views/HomeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreenView.swift
+//  NoBetAppReal
+//
+//  Created by Carsen Werbes on 8/4/25.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreenView()
+}
